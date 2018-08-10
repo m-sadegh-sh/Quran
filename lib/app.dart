c41581916fcdfbb38f6daf1c822b07d84e9f7c1d
NET Guy <m.sadegh.sh@gmail.com>
2018-08-10 22:38:48 +0430

    commit

--- lib/app.dart
+++ lib/app.dart
@@ -4,29 +4,29 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'screens/home/index.dart';
 
 class QuranApp extends StatelessWidget {
-    final routes = <String, WidgetBuilder>{
+  final routes = <String, WidgetBuilder>{
     '/Home': (BuildContext context) => new Home()
   };
 
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'قرآن',
-      theme: new ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.tealAccent[6],
-        accentColor: Colors.cyan[600],
-        fontFamily: 'IranSans'
-      ),
-      initialRoute: 'Home',
-      routes: routes,
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('fa', 'IR'),
-      ]
-    );
+        title: 'قرآن',
+        theme: new ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.teal,
+          accentColor: Colors.tealAccent,
+          fontFamily: 'IranSans'
+        ),
+        initialRoute: 'Home',
+        routes: routes,
+        home: new Home(title: 'a'),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('fa', 'IR'),
+        ]);
   }
 }
