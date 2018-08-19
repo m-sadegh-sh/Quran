d704809d4d59ee65abf1f89686cf8368dedd5a9f
NET Guy <m.sadegh.sh@gmail.com>
2018-08-19 21:18:41 +0430

    fix app

--- lib/app.dart
+++ lib/app.dart
@@ -5,22 +5,31 @@ import 'package:Quran/app_localizations.dart';
 import 'package:Quran/screens/home_screen.dart';
 
 class QuranApp extends StatelessWidget {
-  final routes = <String, WidgetBuilder>{'/': (BuildContext context) => HomeScreen()};
+  _getRoutes(BuildContext context) => <String, WidgetBuilder>{
+    '/': (BuildContext context) => HomeScreen()
+  };
+
+  _createTheme(BuildContext context) => ThemeData(
+    primarySwatch: Colors.teal,
+    accentColor: Colors.tealAccent,
+    fontFamily: 'IranSans'
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        onGenerateTitle: (context) => AppLocalizations.of(context).translate('app-title'),
-        theme: ThemeData(primarySwatch: Colors.teal, accentColor: Colors.tealAccent, fontFamily: 'IranSans'),
-        initialRoute: '/',
-        routes: routes,
-        localizationsDelegates: [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        supportedLocales: [
-          const Locale('fa', 'IR'),
-        ]);
+      onGenerateTitle: (context) => AppLocalizations.of(context).translate('app-title'),
+      theme: _createTheme(context),
+      initialRoute: '/',
+      routes: _getRoutes(context),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('fa', 'IR'),
+      ]
+    );
   }
 }
