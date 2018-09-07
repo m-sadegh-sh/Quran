1a3637ecd04c2634cf6098efdc2405e058570506
NET Guy <m.sadegh.sh@gmail.com>
2018-09-07 22:25:13 +0430

    ci

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:redux_logging/redux_logging.dart';
 import 'package:Quran/states/root_state.dart';
 import 'package:Quran/reducers/root_reducer.dart';
 import 'package:Quran/containers/app_container.dart';
-import 'package:Quran/middlewares/chapter_middleware.dart';
+import 'package:Quran/middlewares/chapter_list_middleware.dart';
 
 void main() {
   final store = new Store<RootState>(
