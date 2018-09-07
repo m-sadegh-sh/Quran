147bac89947bfec3fd4c469d15dae89c167aa4a1
NET Guy <m.sadegh.sh@gmail.com>
2018-09-08 01:08:01 +0430

    ci

--- lib/presentation/chapter_list_item.dart
+++ lib/presentation/chapter_list_item.dart
@@ -13,9 +13,37 @@ class ChapterListItem extends StatelessWidget {
     this.chapterListOnChapterItemTapped
   }) : super(key: key);
 
+  Widget _buildChapterNumber(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 10.0),
+      child: SizedBox(
+        width: 48.0,
+        height: 48.0,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColorLight,
+            borderRadius: BorderRadius.circular(180.0)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                chapterItem.id.toString(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Theme.of(context).primaryColorDark
+                )              
+              )
+            ]
+          )
+        )
+      )
+    );
+  }
+
   Widget _buildChapterTitle() {
     return Container(
-      padding: const EdgeInsets.only(bottom: 5.0),
       child: Text(
         chapterItem.title,
         style: TextStyle(
@@ -43,20 +71,68 @@ class ChapterListItem extends StatelessWidget {
     );
   }
 
+  Widget _buildChapterOrder(BuildContext context) {
+    var description = AppLocalizations.of(context).translateFormatted(
+      "chapter-item-order",
+      {
+        "order": chapterItem.order
+      }        
+    );
+
+    return Text(
+      description,
+      style: TextStyle(
+        fontWeight: FontWeight.w200,
+        fontSize: 14.0
+      )
+    );
+  }
+
+  Widget _buildChapterPartNumber(BuildContext context) {
+    var description = AppLocalizations.of(context).translateFormatted(
+      "chapter-item-part-number",
+      {
+        "partNumber": chapterItem.partNumber
+      }        
+    );
+
+    return Text(
+      description,
+      style: TextStyle(
+        fontWeight: FontWeight.w200,
+        fontSize: 14.0
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => chapterListOnChapterItemTapped(context, chapterItem),
       child: Container(
         padding: const EdgeInsets.all(10.0),
-        child: Row(
+        child: Row(          
           children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                _buildChapterTitle(),
-                _buildChapterDescription(context)
-              ],
+            _buildChapterNumber(context),
+            Expanded(
+              flex: 3,
+              child: Column(                
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _buildChapterTitle(),
+                  _buildChapterDescription(context)
+                ],
+              )
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(                
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  _buildChapterPartNumber(context),
+                  _buildChapterOrder(context)
+                ],
+              )
             )
           ],
         )
