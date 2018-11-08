1131c0fb32a0883533af829ecc1e9859d47bcc5f
NET Guy <m.sadegh.sh@gmail.com>
2018-11-08 14:20:57 +0330

    update packages

--- lib/middlewares/chapter_details_middleware.dart
+++ lib/middlewares/chapter_details_middleware.dart
@@ -3,7 +3,6 @@ import 'package:redux/redux.dart';
 
 import 'package:Quran/states/root_state.dart';
 import 'package:Quran/actions/chapter_details_action.dart';
-import 'package:Quran/data/chapter_repository.dart';
 
 List<Middleware<RootState>> createChapterDetailsMiddleware() {
   return [
