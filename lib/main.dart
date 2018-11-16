9a3c2a1e6d8c1db08f5bac4709670edad2c387f0
NET Guy <m.sadegh.sh@gmail.com>
2018-11-16 21:13:35 +0330

    redux_persist

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:logging/logging.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_logging/redux_logging.dart';
+import 'package:redux_persist/redux_persist.dart';
+import 'package:redux_persist_flutter/redux_persist_flutter.dart';
 
 import 'package:quran/states/root_state.dart';
 import 'package:quran/reducers/root_reducer.dart';
@@ -11,19 +13,40 @@ import 'package:quran/middlewares/chapter_list_middleware.dart';
 import 'package:quran/middlewares/chapter_details_middleware.dart';
 
 void main() async {
-  final store = new Store<RootState>(
-    rootReducer,
-    initialState: RootState.initial(),
-    middleware: [
-      new LoggingMiddleware.printer(level: Level.SHOUT)
-    ]..addAll(createChapterListMiddleware())
-     ..addAll(createChapterDetailsMiddleware())
-  );
+  final persistor = _createPersistor();
+  final store = _createStore(persistor);
+  
+  persistor.load(store);
 
   runApp(
     StoreProvider(
       store: store,
-      child: AppContainer()
+      child: PersistorGate(
+        persistor: persistor,
+        builder: (context) => AppContainer()
+      )
     )
+  );
+}
+
+Persistor<RootState> _createPersistor() {
+  return Persistor<RootState>(
+    version: 1,
+    storage: FlutterStorage(
+      "quran",      
+      location: FlutterSaveLocation.sharedPreferences
+    )
+  );
+}
+
+Store<RootState> _createStore(Persistor<RootState> persistor) {
+  return new Store<RootState>(
+    rootReducer,
+    initialState: RootState.initial(),
+    middleware: [
+      persistor.createMiddleware(),
+      new LoggingMiddleware.printer(level: Level.SHOUT)
+    ]..addAll(createChapterListMiddleware())
+    ..addAll(createChapterDetailsMiddleware())
   );
 }
